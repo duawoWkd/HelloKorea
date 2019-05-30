0087abfa7be1e9f5127c188340cfa00d4197c95f
duawoWKd <duawowkd@naver.com>
2019-05-30 16:37:29 +0900

    first

---
+++ src/HelloKorea.java
@@ -0,0 +1,23 @@
+
+public class HelloKorea { 										//클래스 소스파일
+
+	public static void main(String[] args) {					//H.k소스속에는 main이 들어있다
+		
+		
+		
+		System.out.println("우리나라만세");						//한문장 취급 ;<-문장끝 의미main속 문장
+		System.out.println("우리나라만세");						// 소스코드 제작= 프로그래밍 = 코딩
+		System.out.println("Korea");
+		System.out.println(30+40);
+		System.out.println("30+40");
+		System.out.println("대한민국만세");
+		System.out.println("====================");
+		System.out.println("이름   전화번호  주소");
+		System.out.println("--------------------");
+		System.out.println("홍길동  011   서울시");
+		System.out.println("이몽룡  002   남원시");
+		System.out.println("====================");
+		
+	}
+
+}
